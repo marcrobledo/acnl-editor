357175ca45714fb3bb857016ef064cedcdaf8ed6
marcrobledo <mark@tinet.org>
2019-04-27 17:49:16 +0200

    fixed building list in available PWPs

--- _cache_service_worker.js
+++ _cache_service_worker.js
@@ -30,7 +30,7 @@ caches.keys().then(function(cacheNames){
 });
 
 var PRECACHE_ID='acnl-editor';
-var PRECACHE_VERSION='v1';
+var PRECACHE_VERSION='v1b';
 var PRECACHE_URLS=[
 	'/acnl-editor/','/acnl-editor/index.html',
 	'/acnl-editor/help.html',
